c31dbdb61db78c70360b7e02c0454e382f54ae1d
Edwin Rozo - edwindev <erozog125@gmail.com>
2025-04-22 12:18:20 -0500

    Instrumento de evaluación

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import { FilterProducts } from "./components/FilterProducts"
+
+function App() {
+  return (
+    <>      
+      <FilterProducts />
+    </>
+  );
+}
+
+export default App
