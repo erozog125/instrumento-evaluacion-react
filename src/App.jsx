e1af9ced96c7406217c4f0655fe79663e16014d9
Sena <dimatesebas73@gmail.com>
2025-04-25 07:36:03 -0500

    Estructura

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import "./App.jsx";
+import React from "react";
+import { Product } from "./components/Product/Product.jsx";
 
-function App() {
-  
+export default function App() {
   return (
-    <>
-     <></>
-     <h1>Hola</h1>
-    </>
+    <div>
+      <Product
+        sku="DEP001"
+        nameProduct="Bicicleta de montaña"
+        category="Deportes"
+        price="1200000"
+        description="Bicicleta ideal para senderismo y rutas difíciles."
+      />
+
+      <Product
+        sku="DEP002"
+        nameProduct="Balón de fútbol"
+        category="Deportes"
+        price="150000"
+        description="Balón de fútbol de alta calidad para entrenamientos y partidos."
+      />
+
+      <Product
+        sku="DEP003"
+        nameProduct="Raqueta de tenis"
+        category="Deportes"
+        price="250000"
+        description="Raqueta profesional para jugadores de nivel avanzado."
+      />
+    </div>
   );
 }
-
-export default App;
-
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,29 +1,22 @@
 import React from "react";
 
-export const Product = (currentProduct) => {
-  const { sku, nameProduct, category, price, description } = currentProduct;
-
+export const Product = ({ sku, nameProduct, category, price, description }) => {
   return (
     <div className="mt-2 max-w-sm mx-auto border border-solid rounded-lg p-4 bg-gray-50 shadow-lg text-blue-700">
-      {/* Título del producto */}
-      <h2 className="text-xl font-semibold mb-2 text-center">{nameProduct}</h2>
+      <h1 className="text-lg font-bold mb-2">{nameProduct}</h1>
 
-      {/* SKU */}
       <p className="text-sm font-medium mb-1">
         SKU: <span className="font-normal">{sku}</span>
       </p>
 
-      {/* Categoría */}
       <p className="text-sm font-medium mb-1">
         Categoría: <span className="font-normal">{category}</span>
       </p>
 
-      {/* Precio */}
       <p className="text-sm font-medium mb-1">
         Precio: <span className="font-normal">{price}</span>
       </p>
 
-      {/* Descripción */}
       <p className="text-sm font-medium mb-1">
         Descripción: <span className="font-normal">{description}</span>
       </p>
