d190618cb7aaf0e888d44efb9d88d517c96cb1cd
Sena <dimatesebas73@gmail.com>
2025-04-25 06:47:07 -0500

    Error FilteredProducts CHECK 1/3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
 import { FilterProducts } from "./components/FilterProducts";
 
-function App() {
+export function App() {
   
   return (
     <>
-     <></>
-      <FilterProducts/>
+    <FilterProducts/>
+      
     </>
   );
 }
 
-export default App;
+
 
