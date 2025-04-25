512c9d59bf4cc855b1391ea70d89f07da0b713b5
Zamara-Osorio <zamaraosorio88@gmail.com>
2025-04-25 07:31:21 -0500

    primer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,24 @@ import "./App.css";
 import { FilterProducts } from "./components/FilterProducts";
 
 function App() {
+  const[min, setMinPrice] = useState(0);
+  const[max, setMaxPrice] = useState(1000);
+  const [FilterProducts, setFilteredProducts] = useState(products);
+
+  const handlePriceFilter = (min, max) => {
+    const min =parseInt(minPrice);
+    const max =parseInt(maxPrice);
+
+    const filteredProducts = products.filter(
+      (product) => product.price >= min && product.price <= max
+    );
+    setFilteredProducts(filteredProducts);
+  };
+  const handleReset = () => {
+    setFilteredProducts(products);
+    setMinPrice(0);
+    setMaxPrice(2000);
+  };
   
   return (
     <>
